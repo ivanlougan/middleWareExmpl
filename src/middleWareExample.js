8b169fc1babbcf15871989c3ddeef869dff3dce1
Robert Zdeb <robinio123@gmail.com>
2023-03-06 10:20:07 +0000

    server setup

---
+++ src/middleWareExample.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const port = process.env.PORT || 5001;
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ message: "api is working" })
+})
+
+app.listen(port, () => {
+    console.log(`app listening on post ${port}`);
+})
